2b6b4b9a44770842d96d09a96353a4cbd9126d7d
Luc Libralesso <libralesso.l@gmail.com>
2021-12-26 12:57:53 +0100

    update benchs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod set_store_bench;
 mod pareto_pq_bench;
 
 fn main() {
+    println!("running benchmarks...");
     pareto_pq_bench::pareto_pq_bench();
     // set_store_bench::set_store_benchmark(100,10000);
 }
+
--- src/pareto_pq_bench.rs
+++ src/pareto_pq_bench.rs
@@ -1,26 +1,28 @@
 use std::time::Instant;
 
+use do_util::priority_queue::PriorityQueue;
 use rand::Rng;
 use rand::prelude::ThreadRng;
 
-use do_util::pareto_pq::{
-    ParetoElement, ParetoFront,
+use do_util::priority_queue::{
+    ParetoElement,
+    GuidedElement,
     util::CartesianParetoElement
 };
-use do_util::pareto_pq::list::ListParetoFront;
+use do_util::priority_queue::pareto_pq_list::ListParetoFront;
 // use do_util::pareto_pq::naive_kd_tree::NaiveKDTreeFront;
-use do_util::pareto_pq::kd_tree::KDTreeFront;
+// use do_util::pareto_pq::kd_tree::KDTreeFront;
 
 pub fn random_element<const NB_DIM:usize>(rng:&mut ThreadRng) -> CartesianParetoElement<NB_DIM> {
     let mut coords = [0;NB_DIM];
     loop {
         for i in coords.iter_mut().take(NB_DIM) {
-            *i = rng.gen();
+            *i = rng.gen::<u32>() % 10000;
         }
         if coords.iter().map(|e| {
-            (*e as f64 / u16::MAX as f64).powf(NB_DIM as f64)
+            (*e as f64 / u32::MAX as f64).powf(NB_DIM as f64)
         }).sum::<f64>() <= 1. {
-            let mut new_coords = [u16::MAX ; NB_DIM];
+            let mut new_coords = [10000 ; NB_DIM];
             for i in 0..NB_DIM {
                 new_coords[i] -= coords[i];
             }
@@ -34,8 +36,8 @@ pub fn random_element<const NB_DIM:usize>(rng:&mut ThreadRng) -> CartesianPareto
 pub fn perform_bench<'a, T, Elt:ParetoElement<T>, Front>
 (elements:&[Elt], mut front:Front) where
 T:Ord+std::fmt::Debug,
-Elt:'a + ParetoElement<T>+Eq+Clone,
-Front:ParetoFront<T, Elt> {
+Elt:'a + ParetoElement<T>+GuidedElement<T>+Eq+Clone,
+Front:PriorityQueue<T, Elt> {
     // inserts elements
     let start_insert = Instant::now();
     for e in elements {
@@ -47,22 +49,15 @@ Front:ParetoFront<T, Elt> {
     );
     // pop them until no more elements in the front
     let mut nb_pops:usize = 0;
-    let mut dim:usize = 1;
     let start_pop = Instant::now();
-    let mut last = front.peek_minimum_coordinate(dim).unwrap();
-    let mut dim_vec = Vec::new();
-    while let Some(e) = front.pop_minimum_element(dim) {
+    let mut last = front.peek_minimum().unwrap().guide();
+    while let Some(e) = front.pop_minimum() {
         nb_pops += 1;
-        if last > e.kth(dim) {
-            println!("error");
+        if last > e.guide() {
+            panic!("error: priority queue did not extract a minimum element.");
         }
-        last = e.kth(dim);
-        dim_vec.push(e.kth(dim));
-        // println!("{:?}", e.kth(dim));
-        // println!("{:?}", front.peek_minimum_coordinate(dim));
+        last = e.guide();
     }
-    dim_vec.sort();
-    println!("{:?}", dim_vec);
     let time_pop = start_pop.elapsed().as_secs_f32();
     println!("\t{:<5} pops in {} seconds ({} pops/s)", nb_pops, time_pop, nb_pops as f32/time_pop);
 }
@@ -76,15 +71,16 @@ pub fn bench_pareto<const NB_DIM:usize>(nb_elts:usize) {
     }
     // TEST LIST PARETO
     println!("List structure:");
-    let list_front:ListParetoFront<CartesianParetoElement<NB_DIM>> = ListParetoFront::default();
+    let list_front:ListParetoFront<u32,CartesianParetoElement<NB_DIM>> = ListParetoFront::default();
     perform_bench(&elements, list_front);
-    // TEST KD-TREE
-    println!("kd-tree structure:");
-    let kdtree_front:KDTreeFront<u16, CartesianParetoElement<NB_DIM>, NB_DIM> = KDTreeFront::default();
-    perform_bench(&elements, kdtree_front);
+    // // TEST KD-TREE
+    // println!("kd-tree structure:");
+    // let kdtree_front:KDTreeFront<u16, CartesianParetoElement<NB_DIM>, NB_DIM> = KDTreeFront::default();
+    // perform_bench(&elements, kdtree_front);
 }
 
 
 pub fn pareto_pq_bench() {
-    bench_pareto::<3>(100);
+    println!("running pareto priority queue benchmark...");
+    bench_pareto::<9>(100000);
 }
